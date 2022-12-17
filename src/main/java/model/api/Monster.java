e65db020e21d3c07621fae406bac61dec0b7cea6
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2022-12-17 18:32:09 +0100

    Finished Monster; TODO spells

--- src/main/java/model/api/Monster.java
+++ src/main/java/model/api/Monster.java
@@ -1,5 +1,7 @@
 package model.api;
 
+import control.api.Stat;
+
 /**Models any monster.
  * @see Creature
  */
@@ -25,6 +27,25 @@ public interface Monster extends Creature {
      */
     public String getInfo();
 
-    /**Returns the result of a roll */
+    /**Returns the result of a roll in the specified {@link control.api.Stat}.
+     * @param statistic the stat used as modifier for the saving throw
+     * @return the result of the saving throw
+    */
     public int savingThrow(final Stat statistic);
+
+    /**Heals the monster by the amount specified.
+     * @param gained healing of the monster
+     */
+    public void gainHP(final int gained);
+
+    /**The monster takes damage based on the amount specified, applying the modifier based on his resistances.
+     * @param lost damage taken
+     * @param resistance modifier based on monster resistances
+     */
+    public void loseHP(final int lost, final double resistance);
+
+    /*TODO
+     *  public String getSpells();
+     */
+   
 }
