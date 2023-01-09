a062ee56d5e0b4dfe830f0e5eddf3021875e13ab
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-09 14:19:33 +0100

    Setup fields in MonsterBuilder

--- src/main/java/model/impl/MonsterBuilder.java
+++ src/main/java/model/impl/MonsterBuilder.java
@@ -1,5 +1,24 @@
 package model.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.api.Monster.Element;
+import model.api.Monster.Stat;
+
 public class MonsterBuilder {
+    private final String name;
+    private int ac;
+    private final Map<Stat, Integer> stats = new HashMap<>();
+    private final Map<Stat, Integer> savingThrows = new HashMap<>();
+    private final Map<Element, Double> resistances = new HashMap<>();
+    private int hpTotal;
+    private String info; //Comprehends movement, senses, languages, skills, special attacks
+    private final List<String> actions = new ArrayList<>();
+    private int numberOfLegendaryActions;
+    private final Map<String, Integer> legendaryActions = new HashMap<>();
+
 
 }
