a359d411cb27455b9f59db44682eac15b01b18e7
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-09 13:44:12 +0100

    Added Dice static methods

---
+++ src/main/java/control/impl/Dice.java
@@ -0,0 +1,29 @@
+package control.impl;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Dice {
+
+    public static final int ADVANTAGE_DISADVANTAGE = 2;
+
+    private static Stream<Integer> rollMany(final Die d, final int quantity) {
+        return Stream.generate(() -> d.roll()).limit(quantity);
+    }
+    
+    public static int rollAdvantage(final Die d) {
+        return rollMany(d, ADVANTAGE_DISADVANTAGE).max(Integer::compareTo).get();
+    }
+
+    public static int rollDisadvantage(final Die d) {
+        return rollMany(d, ADVANTAGE_DISADVANTAGE).min(Integer::compareTo).get();
+    }
+
+    public static int sumRolls(final Die d, final int quantity) {
+        return rollMany(d, quantity).peek(System.out::println).collect(Collectors.summingInt(i -> i));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum 5 result: " + Dice.sumRolls(Die.D6, 5));
+    }
+}
