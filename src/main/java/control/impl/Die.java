2a9e0a0245502e1c2753de6e015396e44d14152e
Michele Ciavatti <michele.ciavatti2@studio.unibo.it>
2023-01-09 13:29:25 +0100

    Implemented enum Die

--- src/main/java/control/impl/Die.java
+++ src/main/java/control/impl/Die.java
@@ -1,5 +1,24 @@
 package control.impl;
 
+import java.util.Random;
+
 public enum Die {
-    
+    D100(100),
+    D20(20),
+    D12(12),
+    D8(8),
+    D6(6),
+    D4(4),
+    D2(2);
+
+    private final int bound;
+    private final Random rand = new Random();
+
+    private Die(final int bound) {
+        this.bound = bound;
+    }
+
+    public int roll() {
+        return this.rand.nextInt(bound) + 1;
+    }
 }
